f87375d9dbaaa87b6edb21ca3c22c3d0cb3262cf
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2026-03-06 23:43:15 +0500

    feat(products): add server side product filtering

--- src/entities/product/api/useGetProducts.ts
+++ src/entities/product/api/useGetProducts.ts
@@ -1,9 +1,30 @@
 import { httpClient } from '@/shared/api/httpClient'
 import { useQuery } from '@tanstack/vue-query'
 import type { ProductFromServer } from '../model/types.api'
-export const useGetProducts = () => {
+import { computed, type ComputedRef, type MaybeRef } from 'vue'
+
+type ProductsFilters = {
+  search?: string
+  category?: string[]
+  minPrice?: number
+  maxPrice?: number
+}
+
+export const useGetProducts = (filters: ComputedRef<ProductsFilters>) => {
   return useQuery({
-    queryKey: ['products'],
-    queryFn: async () => httpClient<ProductFromServer[]>('products', { method: 'GET' }),
+    queryKey: computed(() => ['products', filters]),
+    queryFn: async () => {
+      const params = new URLSearchParams()
+
+      if (filters.value.search) {
+        params.append('search', filters.value.search)
+      }
+
+      if (filters.value.category) {
+        filters.value.category.forEach((c) => params.append('category', c))
+      }
+
+      return httpClient<ProductFromServer[]>(`products?${params}`, { method: 'GET' })
+    },
   })
 }
--- src/entities/product/ui/ProductsList.vue
+++ src/entities/product/ui/ProductsList.vue
@@ -1,59 +1,48 @@
 <template>
-  <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    <article class="h-full" v-for="p in filteredProducts" :key="p.id">
-      <BaseCard>
-        <template #header>
-          <img :src="p.image" :alt="p.title" class="w-full h-40 object-contain rounded" />
-        </template>
+  <div>
+    <div v-if="isLoading">Loading</div>
+    <div v-if="isError">Error {{ error }}</div>
+    <div v-else class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+      <article class="h-full" v-for="p in productsFromServer" :key="p.id">
+        <BaseCard>
+          <template #header>
+            <img :src="p.image" :alt="p.title" class="w-full h-40 object-contain rounded" />
+          </template>
 
-        <h3 class="mt-2 font-semibold line-clamp-2">{{ p.title }}</h3>
-        <p class="text-sm text-gray-600 line-clamp-2">{{ p.description }}</p>
+          <h3 class="mt-2 font-semibold line-clamp-2">{{ p.title }}</h3>
+          <p class="text-sm text-gray-600 line-clamp-2">{{ p.description }}</p>
 
-        <template #footer>
-          <div class="flex justify-between">
-            <div class="mt-2 font-semibold text-green-600!">{{ p.price }} $</div>
-            <RouterLink :to="`/product/${p.id}`">
-              <button
-                class="px-3 py-1 bg-zinc-800 text-white! rounded-lg hover:bg-zinc-700 cursor-pointer transition-colors"
-              >
-                Купить
-              </button>
-            </RouterLink>
-          </div>
-        </template>
-      </BaseCard>
-    </article>
+          <template #footer>
+            <div class="flex justify-between">
+              <div class="mt-2 font-semibold text-green-600!">{{ p.price }} $</div>
+              <RouterLink :to="`/product/${p.id}`">
+                <button
+                  class="px-3 py-1 bg-zinc-800 text-white! rounded-lg hover:bg-zinc-700 cursor-pointer transition-colors"
+                >
+                  Купить
+                </button>
+              </RouterLink>
+            </div>
+          </template>
+        </BaseCard>
+      </article>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { products } from '@/app/tempData'
 import BaseCard from '@/shared/ui/BaseCard/BaseCard.vue'
-import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useGetProducts } from '../api/useGetProducts'
-
-const productsFromServer = useGetProducts()
-console.log('productsFromServer', productsFromServer.data)
+import { computed } from 'vue'
 
 const route = useRoute()
 
-const filteredProducts = computed(() => {
-  const search = ((route.query.search as string) || '').toLocaleLowerCase()
-  const category = route.query.category as string[]
-  const minPrice = Number(route.query.minPrice) || 0
-  const maxPrice = Number(route.query.maxPrice) || 0
-
-  return products.filter((p) => {
-    if (search && !p.title.toLocaleLowerCase().includes(search)) return false
-
-    if (category && !category.includes(p.category)) return false
-
-    if (minPrice && p.price < minPrice) return false
-
-    if (maxPrice && p.price > maxPrice) return false
-
-    return true
-  })
-})
+const filters = computed(() => ({
+  search: route.query.search as string | undefined,
+  category: route.query.category as string[] | undefined,
+  minPrice: route.query.minPrice ? Number(route.query.minPrice) : undefined,
+  maxPrice: route.query.maxPrice ? Number(route.query.maxPrice) : undefined,
+}))
+const { data: productsFromServer, isLoading, isError, error } = useGetProducts(filters)
 </script>
